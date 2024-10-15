c9757fc470788af99cd7be764bdf9828cc8f4559
Vikasweg <v3073773@gmail.com>
2024-10-15 19:04:34 +0300

    Add files via upload

---
+++ 110.py
@@ -0,0 +1,65 @@
+
+
+
+
+'''
+s=0
+for i in range (5):
+    a=int(input())
+    s+=a
+print(s)'''
+    
+    
+
+
+'''
+a=int(input())
+if a>1000:
+    a=a-(a*0.1)
+    print(a)
+elif a>500:
+    a=a-(a*0.05)
+    print(a)
+else:
+    print(a)'''
+
+
+
+
+'''
+a=input()
+if a.isdigit():
+    print('Это цифра!')
+elif a.isalpha():
+    print('Это буква!')
+else:
+    print('Это не цифра и не буква!')'''
+
+
+
+
+'''
+a=str(input())
+if a=='secret':
+    print('Доступ разрешен!')
+else:
+    print('Неверный пароль!')'''
+
+
+
+
+'''
+a=int(input())
+if a>14:
+    print('Добро пожаловать')
+elif a==14:
+    print('Вам 14 лет! Поздравляю!')
+else:    
+    print('Вам ещё рано')'''
+
+
+
+
+
+    
+    
